0170b3bcb47c97d9307713d6d7cd6cfb31807c8b
N00b13-M1 <michael.cornelis@gmail.com>
2022-09-03 17:45:13 +0200

    Test_mc

--- test.js
+++ test.js
@@ -96,3 +96,150 @@ console.log(firstEpisode);
 // thirdEpisode
 
 // Remember to pass appropriate arguments to each!
+
+// Create an array stored in a variable called episodes, and populate it with the three variables firstEpisode, secondEpisode and thirdEpisode. Watch the page populate itself automatically!
+
+// Try changing the order in which you add the episodes, and watch what happens as it refreshes.
+
+let episodes = ["firstEpisode", 'secondEpisode', 'thirdEpisode']
+
+
+// var obj = { first: 'reference' };
+// obj.push = ('test')
+
+// console.log(obj);
+
+
+var reference = [1];
+var refCopy = reference;
+reference.push(2);
+console.log(reference, refCopy); 
+
+var hundred = 100;
+var two = 2;
+function multiply(x, y) {
+    // PAUSE
+    return x * y;
+}
+var twoHundred = multiply(hundred, two);
+console.log(twoHundred);
+
+
+// function changeAgeImpure(person) {
+//     person.age = 25;
+//     return person;
+// }
+
+// console.log(person);
+
+// function changeAgePure(person) {
+//     var newPersonObj = JSON.parse(JSON.stringify(person));
+//     newPersonObj.age = 25;
+//     return newPersonObj;
+// }
+// var alex = {
+//     name: 'Alex',
+//     age: 30
+// };
+// var alexChanged = changeAgePure(alex);
+// console.log(alex); 
+// console.log(alexChanged); 
+
+
+// var personObj1 = {
+//     name: 'Alex',
+//     age: 30
+// };
+// var person = personObj1;
+// person.age = 25;
+
+// console.log(person);
+// console.log(personObj1);
+
+
+// var arr1 = ['Hi!'];
+// var arr2 = ['Hi!'];
+// console.log(arr1 === arr2);
+
+
+// var arr1str = JSON.stringify(arr1);
+// var arr2str = JSON.stringify(arr2);
+// console.log(arr1str === arr2str);
+
+
+var obj = { first: 'reference' };
+obj.second = 'ref2';
+
+console.log(obj);
+
+
+var arrRef = ['Hi!'];
+var arrRef2 = arrRef;
+console.log(typeof arrRef );
+console.log(arrRef === arrRef2);
+
+var arr1 = ['Hi!'];
+var arr2 = ['Hi!'];
+console.log(arr1 === arr2);
+
+var arr1str = JSON.stringify(arr1);
+var arr2str = JSON.stringify(arr2);
+console.log(arr1str);
+console.log(arr2str);
+console.log(arr1str === arr2str);
+
+// function changeAgeImpure(person) {
+//     person.age = 25;
+//     return person;
+// }
+// var alex = {
+//     name: 'Alex',
+//     age: 30
+// };
+// var changedAlex = changeAgeImpure(alex);
+// console.log(alex); 
+// console.log(changedAlex);
+
+// function changeAgePure(person) {
+//     var newPersonObj = JSON.parse(JSON.stringify(person));
+//     newPersonObj.age = 25;
+//     return console.log(newPersonObj);
+// }
+// var alex = {
+//     name: 'Alex',
+//     age: 30
+// };
+// changeAgePure(alex);
+// // console.log(alex); // -> { name: 'Alex', age: 30 }
+// // console.log(alexChanged); 
+
+function changeAgeAndReference(person) {
+    person.age = 25;
+    person = {
+        name: 'John',
+        age: 50
+    };
+    
+    return person;
+}
+var personObj1 = {
+    name: 'Alex',
+    age: 30
+};
+changeAgeAndReference(personObj1);
+console.log(personObj1); // -> ?
+// console.log(personObj2); // -> ?
+
+
+// var personObj1 = {
+//     name: 'Alex',
+//     age: 30
+// };
+
+// personObj1.age = 25;
+// personObj1 = {
+//     name: 'John',
+//     age: 50
+// };
+
+// console.log(personObj1);
